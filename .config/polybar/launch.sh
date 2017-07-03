bf42a0e41e0d37d26e98398a022de8d245d9649b
cj360 <ayunker551@gmail.com>
2017-07-03 19:31:54 -0400

    update polybar launch script
    Signed-off-by: cj360 <ayunker551@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # Terminate already running bar instances
-killall polybar
+killall -q polybar
+
 # Wait until the processes have been shut down
-#while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
 # Launch bars
-polybar polybar &
-#polybar top &
-#polybar bottom &
+polybar Bar &
