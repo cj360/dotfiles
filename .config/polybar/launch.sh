7534d88b58a123bdd593a7afaacb25186527e20d
cj360 <ayunker551@gmail.com>
2017-07-30 13:35:17 -0400

    update polybar, i3 to read xrdb colors from 'bg' supplied to wal script
    Signed-off-by: cj360 <ayunker551@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 # Terminate already running bar instances
-killall -q polybar
+killall polybar
+#killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-polybar Bar &
+polybar rome &
