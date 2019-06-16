566d15709a02a896cc614400eadee502d27065a8
cj360 <ayunker551@gmail.com>
2019-06-16 15:06:47 -0400

    dotfiles: update configs for tk-glitch's pkgbuilds update lock script for more up to date i3lock https://github.com/PandorasFox/i3lock-color
    Signed-off-by: cj360 <ayunker551@gmail.com>

--- .config/lock_distort.sh
+++ .config/lock_distort.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 tmpbg="/tmp/screen.png"
 scrot "$tmpbg"; corrupter -mag 1 "$tmpbg" "$tmpbg"
-i3lock -i "$tmpbg"; rm "$tmpbg"
+i3lock -k --timestr="%I:%M" -i "$tmpbg"; rm "$tmpbg"
