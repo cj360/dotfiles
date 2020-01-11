ef394c793ba1b87941a221da0b8598a5568a6ec6
cj360 <ayunker551@gmail.com>
2020-01-11 16:21:29 -0500

    dotfiles: update mpd, dunst, and .gitconfig

--- .config/wal/dunst.sh
+++ .config/wal/dunst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Symlink dunst config
-ln -sf ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
+mkdir -p  "${HOME}/.config/dunst"
+ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
 
 # Restart dunst with the new color scheme
 pkill dunst
