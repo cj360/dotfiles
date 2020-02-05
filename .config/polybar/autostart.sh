bbcc306baabb420a69ec99b20616aec4684a96c6
cj360 <ayunker551@gmail.com>
2020-02-04 22:22:59 -0500

    dots: update walls & wip polybar

--- .config/polybar/autostart.sh
+++ .config/polybar/autostart.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-#killall -q polybar
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-#echo "---" | tee -a /tmp/polybar1.log
-#polybar example >>/tmp/polybar1.log 2>&1 &
-#polybar bclock >>/tmp/polybar2.log 2>&1 &
-#polybar bdate >>/tmp/polybar3.log 2>&1 &
-#echo "Bars up."
 
 # Terminate already running bar instances
 killall -q polybar
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-echo "---" | tee -a /tmp/polybar.log
-polybar -c ~/.config/polybar/config1 top >>/tmp/polybar.log 2>&1 &
+echo "---" | tee -a /tmp/polybar-current.log
+polybar -c ~/.config/polybar/config-current top >>/tmp/polybar-current.log 2>&1 &
 echo "Bars launched"
